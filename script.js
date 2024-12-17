ab6a92de65e304b651ca731d873ae920ea411c92
flocean <florentpuy@gmail.com>
2024-12-17 13:08:35 -0400

    commit test

---
+++ script.js
@@ -0,0 +1,26 @@
+// Récupération des éléments
+const photos = document.querySelectorAll('.photo img');
+const modal = document.getElementById('imageModal');
+const modalImage = document.getElementById('modalImage');
+const modalClose = document.getElementById('modalClose');
+
+// Ajout de l'événement au clic sur chaque image
+photos.forEach(photo => {
+    photo.addEventListener('click', () => {
+        const fullImageUrl = photo.getAttribute('data-full');
+        modalImage.src = fullImageUrl;
+        modal.style.display = 'flex';
+    });
+});
+
+// Fermeture de la modale
+modalClose.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+// Fermeture en cliquant en dehors de l'image
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+});
