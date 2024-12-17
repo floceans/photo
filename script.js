fd99c877dbdb5707dc18a7785ec8781c234154c5
flocean <florentpuy@gmail.com>
2024-12-17 16:21:51 -0400

    js

--- script.js
+++ script.js
@@ -24,3 +24,19 @@ modal.addEventListener('click', (e) => {
         modal.style.display = 'none';
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const photos = document.querySelectorAll('.photo img');
+
+    photos.forEach(photo => {
+        const img = photo;
+        const width = img.naturalWidth;
+        const height = img.naturalHeight;
+
+        // Si l'image est plus haute que large, ajouter la classe "landscape"
+        if (height > width) {
+            img.parentElement.classList.add('landscape');
+        }
+    });
+});
